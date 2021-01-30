d6b04ea6d2eaf5e43a7c978e968b314adce20d68
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-01-30 17:50:14 +0000

    select role quieries for role title

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ connection.connect(function(err) {
             type: "list",
             message: "What would you like to do?",
             name: "choice",
-            choices; [
+            choices: [
                 "View All Employees?",
                 "View All Employees By Role?",
                 "View All Employees By Departments",
@@ -62,3 +62,52 @@ connection.connect(function(err) {
         }
     })
 })
+// View All Employees //
+
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function(err, res) {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+}
+
+// View All Roles //
+function viewAllRoles() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+    })
+}
+
+// View All Employees By Department // 
+
+function viewAllDepartments() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+    })
+  }
+
+  // Select Role Quieries Role Title for Add Employee Prompt //
+  var roleArr = [];
+  function selectRole() {
+    connection.query("SELECT * FROM role", function(err, res) {
+      if (err) throw err
+      for (var i = 0; i < res.length; i++) {
+        roleArr.push(res[i].title);
+      }
+  
+    })
+    return roleArr;
+  }
+
+  
+
+
+
