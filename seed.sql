f22e24636b69b5504db7b233111676c7a8c71899
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-01-30 17:20:57 +0000

    populated file

--- seed.sql
+++ seed.sql
@@ -1,5 +1,45 @@
 USE employee_trackerDB;
 
-INSERT INTO department (name)
-VALUE ('Finance'),('HR'),('Sales');
+-- DEPARTMENT SEEDS -- 
 
+INSERT INTO department (name)
+VALUE ('Finance'),('Engineering'),('Sales'),('Legal'),;
+
+-- EMPLOYEE ROLE SEEDS --
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead ENgineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+
+-- EMPLOYEE SEEDS -------
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Janet", "Large", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Hayley", "Morris", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Ella","Higgins",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Bob", "Lao", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Chris", "Walsh", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Steven", "Gerrard", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Tom", "Holland", 2, 7);
+
+-- SELECTING FOR CREATING 
+--TABLES IN OUR SQL WORKBENCH 
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
